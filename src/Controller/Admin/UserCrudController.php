7f37ab01a8244c4611d514f863b813869e91b121
Trapo6x7 <matoufdanslezoo@gmail.com>
2025-02-12 14:51:13 +0100

    ADD : ADMIN CREATE USER

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -4,25 +4,60 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Doctrine\ORM\EntityManagerInterface;
 
 class UserCrudController extends AbstractCrudController
 {
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
     public static function getEntityFqcn(): string
     {
         return User::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('email', 'Email'),
+            TextField::new('password', 'Password')
+                ->onlyOnForms()
+                ->setRequired(false), 
+            ChoiceField::new('roles', 'Roles')
+                ->setChoices([
+                    'User' => 'ROLE_USER',
+                    'Admin' => 'ROLE_ADMIN',
+                    'Recruteur' => 'ROLE_RECRUITER',
+                    'Candidat' => 'ROLE_CANDIDATE',
+                ])
+                ->allowMultipleChoices()
+                ->renderExpanded(),
         ];
     }
-    */
+    public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        if ($entityInstance instanceof User && $entityInstance->getPassword()) {
+            $hashedPassword = $this->passwordHasher->hashPassword($entityInstance, $entityInstance->getPassword());
+            $entityInstance->setPassword($hashedPassword);
+        }
+
+        parent::persistEntity($entityManager, $entityInstance);
+    }
+
+    public function updateEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        if ($entityInstance instanceof User && $entityInstance->getPassword()) {
+            $hashedPassword = $this->passwordHasher->hashPassword($entityInstance, $entityInstance->getPassword());
+            $entityInstance->setPassword($hashedPassword);
+        }
+
+        parent::updateEntity($entityManager, $entityInstance);
+    }
 }
